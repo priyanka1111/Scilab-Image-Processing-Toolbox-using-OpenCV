5d4175a699161b4dcc53ef05d08ccbfa3bf67a88
priyanka1111 <you@example.com>
2015-12-14 22:45:01 -0500

    indented_code

--- Phase2_functions/functions/dctmtx/dctmtx.cpp
+++ Phase2_functions/functions/dctmtx/dctmtx.cpp
@@ -4,7 +4,6 @@
 /* Syntax : return_n*n_matrix=dctmtx(double n)  */
 
 /* ==================================================================== */
-/* ==================================================================== */
 #include <numeric>
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
@@ -15,8 +14,10 @@
 #include<string.h>
 using namespace cv;
 using namespace std;
-  extern "C"
-  {
+
+
+extern "C"
+{
   #include "api_scilab.h"
   #include "Scierror.h"
   #include "BOOL.h"
@@ -24,78 +25,72 @@ using namespace std;
   #include "../common.h"
   #define PI 3.14
 
-
-  int dctmtx(char *fname, unsigned long fname_len)
+    int dctmtx(char *fname, unsigned long fname_len)
     {
     // Error management variable
-        SciErr sciErr;
+    SciErr sciErr;
     //variable info
-	int iRows		= 0;
-	int iCols		= 0;
-        int piRows		= 0;
-	int piCols		= 0;
-        int* piAddr2=NULL;
-        int* piAddr1=NULL;
-        int* piAddr3=NULL;
-        double *value=NULL;
-        int* piLen		= NULL;
-        char **val;
-        int i,j;
-        int error;
-        double n;
-        Mat img,dst;                     
-         //checking output argument
-          CheckInputArgument(pvApiCtx,1,1);
-          CheckOutputArgument(pvApiCtx,1,1);
-         //checking if number of input arguments
-           sciErr = getVarAddressFromPosition(pvApiCtx,1,&piAddr1);
-           if (sciErr.iErr)
-           {
-                printError(&sciErr, 0);
-                return 0;
-            }
-                 //this function will fetch value of fifth argument
-           error=getScalarDouble(pvApiCtx,piAddr1,&n) ;  
-           if(error!=0)
-           {
-                  sciprint("error in retrieving second argument");
-           } 
-           int k=0;
-           double *mat;
-           int s=n;
-           mat=(double *)malloc(sizeof(double)*n*n);
-           for(int i=0;i<n;i++)
-           {
-                   for(int j=0;j<n;j++)
-                   {
-                       if(i==0)
-                        {
-                            mat[i + s * j]=(1/(sqrt(n)));
-                        }
-                        else 
-                        {
-                              double temp,temp1;
-                              temp1=2/n;
-                              temp=(2*j+1)*i*PI;
-                              temp=temp/(2*n);
-                              temp=(sqrt(temp1))*(cos(temp));
-                              mat[i+ s*j]= temp;
-                              
-                        }
-                     cout<<mat[i+s*j]<<" "  ;
-                   }
-               cout<<"\n";
-           }        
-        sciErr = createMatrixOfDouble(pvApiCtx, nbInputArgument(pvApiCtx) + 1,n,n,mat);
-        free(mat); // Data have been copied into Scilab memory
-       if (sciErr.iErr)
-        {
-        //Make sure everything is cleanup in case of error
+    int iRows=0;
+    int iCols=0;
+    int piRows=0;
+    int piCols=0;
+    int* piAddr2=NULL;
+    int* piAddr1=NULL;
+    int* piAddr3=NULL;
+    double *value=NULL;
+    int* piLen=NULL;
+    char **val;
+    int i,j;
+    int error;
+    double n;
+    Mat img,dst;                     
+    //checking input and output argument
+    CheckInputArgument(pvApiCtx,1,1);
+    CheckOutputArgument(pvApiCtx,1,1);
+    sciErr = getVarAddressFromPosition(pvApiCtx,1,&piAddr1);
+    if (sciErr.iErr)
+    {
         printError(&sciErr, 0);
         return 0;
-        } 
-        AssignOutputVariable(pvApiCtx, 1) = nbInputArgument(pvApiCtx) + 1;
-        ReturnArguments(pvApiCtx);
-        return 0;    
-}
+    }
+    //this function will fetch value of first argument
+    error=getScalarDouble(pvApiCtx,piAddr1,&n) ;  
+    if(error!=0)
+    {
+         sciprint("error in retrieving second argument");
+    } 
+    int k=0;
+    double *mat;
+    int s=n;
+    mat=(double *)malloc(sizeof(double)*n*n);
+    for(int i=0;i<n;i++)
+    {
+         for(int j=0;j<n;j++)
+           {
+              if(i==0)
+              {
+                  mat[i+s*j]=(1/(sqrt(n)));
+              }
+              else 
+              {
+                  double temp,temp1;
+                  temp1=2/n;
+                  temp=(2*j+1)*i*PI;
+                  temp=temp/(2*n);
+                  temp=(sqrt(temp1))*(cos(temp));
+                  mat[i+ s*j]= temp;      
+               }
+            }
+    }        
+    sciErr = createMatrixOfDouble(pvApiCtx, nbInputArgument(pvApiCtx) + 1,n,n,mat);
+    free(mat);
+    if (sciErr.iErr)
+    {
+      printError(&sciErr, 0);
+      return 0;
+    } 
+    AssignOutputVariable(pvApiCtx, 1) = nbInputArgument(pvApiCtx) + 1;
+    ReturnArguments(pvApiCtx);
+    return 0;    
+    }
 }
--- Phase2_functions/functions/lab2xyz/lab2xyz.cpp
+++ Phase2_functions/functions/lab2xyz/lab2xyz.cpp
@@ -15,8 +15,10 @@
 #include<string.h>
 using namespace cv;
 using namespace std;
-  extern "C"
-  {
+
+
+extern "C"
+{
   #include "api_scilab.h"
   #include "Scierror.h"
   #include "BOOL.h"
@@ -26,33 +28,55 @@ using namespace std;
   #define REF_Y 100.000;
   #define REF_Z 108.883;
 
-struct Color{
-    float R,G,B,X,Y,Z,L,a,b;
-};
-Color lab2xyz1(float l, float a, float b){
+    struct Color
+    {
+     float R,G,B,X,Y,Z,L,a,b;
+    };
+      
+    Color lab2xyz1(float l, float a, float b)
+    {
     float y = (l + 16) / 116;
     float x = a / 500 + y;
     float z = y - b / 200;
-
-    if ( pow( y , 3 ) > 0.008856 ) { y = pow( y , 3 ); }
-    else { y = ( y - 16 / 116 ) / 7.787; }
-    if ( pow( x , 3 ) > 0.008856 ) { x = pow( x , 3 ); }
-    else { x = ( x - 16 / 116 ) / 7.787; }
-    if ( pow( z , 3 ) > 0.008856 ) { z = pow( z , 3 ); }
-    else { z = ( z - 16 / 116 ) / 7.787; }
+    if(pow(y,3) > 0.008856) 
+    { 
+       y = pow(y,3); 
+    }
+    else 
+    {
+       y =(y-16/116)/7.787; 
+    }
+    if (pow(x,3) > 0.008856)
+    {
+       x = pow(x,3);
+    }
+    else
+    { 
+       x = (x-16/116)/7.787;
+    }
+    if (pow(z,3) > 0.008856)
+    {
+       z = pow(z,3); 
+    }
+    else
+    { 
+       z =(z-16/ 116 ) / 7.787; 
+    }
 
     Color xyz;
     xyz.X= x * REF_X;
     xyz.Y = y * REF_Y;
     xyz.Z = z * REF_Z;
     return xyz;
-
-
-} 
- Color lab2xyz2(float l, float a, float b,char *s){
+ 
+    }
+ 
+    Color lab2xyz2(float l, float a, float b,char *s)
+    {
     double REF_X1;
     double REF_Y1;
     double REF_Z1;
+    //if else condition for checking various input arguments and assign values accordingly
     if(!strcmp(s,"a"))
     {
           REF_X1=1.0985; 
@@ -61,25 +85,25 @@ Color lab2xyz1(float l, float a, float b){
     }
     else if(!strcmp(s,"c"))
     {
-             REF_X1=0.9807; 
+          REF_X1=0.9807; 
           REF_Y1= 1.0000;
           REF_Z1=1.1822;
     }
     else if(!strcmp(s,"d50"))
     {
-           REF_X1=0.9642; 
+          REF_X1=0.9642; 
           REF_Y1= 1.0000;
           REF_Z1=0.8251;
     }
-     else if(!strcmp(s,"d65"))
+    else if(!strcmp(s,"d65"))
     {
-            REF_X1=0.9504;
+          REF_X1=0.9504;
           REF_Y1= 1.0000;
           REF_Z1= 1.0888;
     }
     else if(!strcmp(s,"icc"))
     {
-           REF_X1=0.962; 
+          REF_X1=0.962; 
           REF_Y1=1.000;
           REF_Z1= 0.8249;
     }
@@ -92,126 +116,131 @@ Color lab2xyz1(float l, float a, float b){
     float y = (l + 16) / 116;
     float x = a / 500 + y;
     float z = y - b / 200;
-
-    if ( pow( y , 3 ) > 0.008856 ) { y = pow( y , 3 ); }
-    else { y = ( y - 16 / 116 ) / 7.787; }
-    if ( pow( x , 3 ) > 0.008856 ) { x = pow( x , 3 ); }
-    else { x = ( x - 16 / 116 ) / 7.787; }
-    if ( pow( z , 3 ) > 0.008856 ) { z = pow( z , 3 ); }
-    else { z = ( z - 16 / 116 ) / 7.787; }
-
+    if (pow(y,3) > 0.008856)
+    {
+      y=pow(y,3);
+    }
+    else 
+    { 
+      y=(y-16/116)/7.787; 
+    }
+    if(pow(x,3) > 0.008856) 
+    {
+      x=pow(x,3); 
+    }
+    else 
+    { 
+      x=(x-16/116)/7.787; 
+    }
+    if (pow(z,3) > 0.008856)
+    {
+      z=pow(z,3); 
+    }
+    else 
+    {
+      z=(z-16/116)/7.787; 
+    }
     Color xyz;
     xyz.X= x * REF_X1;
     xyz.Y = y * REF_Y1;
     xyz.Z = z * REF_Z1;
     return xyz;
-}  
-  int lab2xyz(char *fname, unsigned long fname_len)
+    }  
+    int lab2xyz(char *fname, unsigned long fname_len)
     {
     // Error management variable
-        SciErr sciErr;
+    SciErr sciErr;
     //variable info
-	int iRows		= 0;
-	int iCols		= 0;
-        int piRows		= 0;
-	int piCols		= 0;
-        int* piAddr=NULL;
-        int* piAddr1=NULL;
-        int* piAddr3=NULL;
-        double *value=NULL;
-        int* piLen		= NULL;
-        char **val;
-        int i,j;
-        int error;
-        Color out;
-        double check;
-        Mat img,dst;
-        float R,G,B;
-        int *piAddrNew = NULL;                      
-         //checking output argument
-          CheckOutputArgument(pvApiCtx,1,1);
-         //checking if number of input arguments are 3
-        
-         int k=0;
-        // SciErr getMatrixOfDoubleInList(void* _pvCtx, int* _piParent, int _iItemPos, int* _piRows, int* _piCols, double** _pdblReal)
-                    double *pstDataR = NULL;
-                    double *pstDataG = NULL;
-                    double *pstDataB = NULL; 
-                    sciErr = getVarAddressFromPosition(pvApiCtx,1,&piAddr);
-                    if(sciErr.iErr)   
-                    {
-                        printError(&sciErr, 0);
-                        return 0;
-                    }
-                    //retrive the matrix of the R values   
-                    sciErr =  getMatrixOfDoubleInList(pvApiCtx, piAddr, 1, &iRows, &iCols, &pstDataR);
-                    if(sciErr.iErr)
-                    {
-                        printError(&sciErr, 0);
-                        return 0;
-                    }
-                    //retrive address of the list
-                    sciErr = getVarAddressFromPosition(pvApiCtx,1,&piAddr);
-                    if(sciErr.iErr)   
-                    {
-                        printError(&sciErr, 0);
-                        return 0;
-                    }   
-                    //retrive the matrix of the G values 
-                    sciErr = getMatrixOfDoubleInList(pvApiCtx, piAddr, 2, &iRows, &iCols, &pstDataG);
-                    if(sciErr.iErr)
-                    {
-                        printError(&sciErr, 0);
-                        return 0;
-                    }
-                    //retrive address of the list
-                    sciErr = getVarAddressFromPosition(pvApiCtx,1,&piAddr);
-                    if(sciErr.iErr)   
-                    {
-                        printError(&sciErr, 0);
-                        return 0;
-                    }   
-                    //retrive the matrix of the B values 
-                    sciErr = getMatrixOfDoubleInList(pvApiCtx, piAddr, 3, &iRows, &iCols, &pstDataB);
-                    if(sciErr.iErr)
-                    {
-                        printError(&sciErr, 0);
-                        return 0;
-                    }
-
-         double *r,*g,*b;
-         int m=0;
-         r=(double *)malloc(sizeof(double)*iRows*iCols);
-         g=(double *)malloc(sizeof(double)*iRows*iCols);
-         b=(double *)malloc(sizeof(double)*iRows*iCols);
-                               for(int i=0;i<iRows;i++)
-                               {
-                                  for(int j=0;j<iCols;j++)
-                                   {
-                                         // Vec3d intensity = img.at<Vec3d>(i,j);
-                                          R= (pstDataR[k]);
-                                          G= (pstDataG[k]);
-                                          B= (pstDataB[k++]);
-                                          out=lab2xyz1(R,G,B);
-                                            cout<<R<<"\n";
-                                            cout<<G<<"\n";
-                                            cout<<B<<"\n";       
-                                            r[m]=(out.X)/100;
-                                            g[m]=(out.Y)/100;
-                                            b[m++]=(out.Z)/100;  
-                                   }
-                               }
-                     
+    int iRows=0;
+    int iCols=0;
+    int piRows=0;
+    int piCols=0;
+    int* piAddr=NULL;
+    int* piAddr1=NULL;
+    int* piAddr3=NULL;
+    double *value=NULL;
+    int* piLen=NULL;
+    char **val;
+    int i,j;
+    int error;
+    Color out;
+    double check;
+    Mat img,dst;
+    float R,G,B;
+    int k=0;
+    int *piAddrNew = NULL; 
+    double *pstDataR=NULL;
+    double *pstDataG=NULL;
+    double *pstDataB=NULL; 
+    double *r,*g,*b;
+    int m=0;                     
+    //checking output argument
+    CheckOutputArgument(pvApiCtx,1,1);
+    //retrive address of the list
+    sciErr=getVarAddressFromPosition(pvApiCtx,1,&piAddr);
+    if(sciErr.iErr)   
+    {
+        printError(&sciErr, 0);
+        return 0;
+    }
+    //retrive the matrix of the R values   
+    sciErr=getMatrixOfDoubleInList(pvApiCtx, piAddr, 1, &iRows, &iCols, &pstDataR);
+    if(sciErr.iErr)
+    {
+        printError(&sciErr, 0);
+        return 0;
+    }
+    //retrive address of the list
+    sciErr = getVarAddressFromPosition(pvApiCtx,1,&piAddr);
+    if(sciErr.iErr)   
+    {
+        printError(&sciErr, 0);
+        return 0;
+    }   
+    //retrive the matrix of the G values 
+    sciErr = getMatrixOfDoubleInList(pvApiCtx, piAddr, 2, &iRows, &iCols, &pstDataG);
+    if(sciErr.iErr)
+    {
+        printError(&sciErr, 0);
+        return 0;
+    }
+    //retrive address of the list
+    sciErr = getVarAddressFromPosition(pvApiCtx,1,&piAddr);
+    if(sciErr.iErr)   
+    {
+        printError(&sciErr, 0);
+        return 0;
+    }   
+    //retrive the matrix of the B values 
+    sciErr = getMatrixOfDoubleInList(pvApiCtx, piAddr, 3, &iRows, &iCols, &pstDataB);
+    if(sciErr.iErr)
+    {
+        printError(&sciErr, 0);
+        return 0;
+    }         
+    r=(double *)malloc(sizeof(double)*iRows*iCols);
+    g=(double *)malloc(sizeof(double)*iRows*iCols);
+    b=(double *)malloc(sizeof(double)*iRows*iCols);
+    for(int i=0;i<iRows;i++)
+    {
+       for(int j=0;j<iCols;j++)
+        {
+           R=(pstDataR[k]);
+           G=(pstDataG[k]);
+           B= (pstDataB[k++]);
+           out=lab2xyz1(R,G,B);      
+           r[m]=(out.X)/100;
+           g[m]=(out.Y)/100;
+           b[m++]=(out.Z)/100;  
+         }
+     }                     
     sciErr = createList(pvApiCtx, nbInputArgument(pvApiCtx) + 1, 3, &piAddrNew);
     if(sciErr.iErr)
     {
         printError(&sciErr, 0);
         return 0;
     }
-
-
     //Adding the R value matrix to the list
-    //Syntax : createMatrixOfInteger32InList(void* _pvCtx, int _iVar, int* _piParent, int _iItemPos, int _iRows, int _iCols, const int* _piData)
     sciErr = createMatrixOfDoubleInList(pvApiCtx, nbInputArgument(pvApiCtx)+1 , piAddrNew, 1, iRows,iCols, r);
     free(r);
     if(sciErr.iErr)
@@ -219,7 +248,6 @@ Color lab2xyz1(float l, float a, float b){
         printError(&sciErr, 0);
         return 0;
     }
-
     //Adding the G value matrix to the list
     sciErr = createMatrixOfDoubleInList(pvApiCtx, nbInputArgument(pvApiCtx)+1 , piAddrNew, 2, iRows, iCols, g);
     free(g);
@@ -228,7 +256,6 @@ Color lab2xyz1(float l, float a, float b){
         printError(&sciErr, 0);
         return 0;
     }
-
     //Adding the B value matrix to the list
     sciErr = createMatrixOfDoubleInList(pvApiCtx, nbInputArgument(pvApiCtx)+1 , piAddrNew, 3, iRows, iCols, b);
     free(b);
@@ -236,11 +263,9 @@ Color lab2xyz1(float l, float a, float b){
     {
         printError(&sciErr, 0);
         return 0;
+    }          
+    AssignOutputVariable(pvApiCtx, 1) = nbInputArgument(pvApiCtx) + 1;
+    ReturnArguments(pvApiCtx);            
+    return 0;                
     }
-
-              
-             AssignOutputVariable(pvApiCtx, 1) = nbInputArgument(pvApiCtx) + 1;
-            ReturnArguments(pvApiCtx);            
-            return 0;                
-}
 }
